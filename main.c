764fccc52f3578e4305738a1db47072a65578136
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2021-11-13 19:36:58 +0545

    beginner 1

---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int T;
+    printf("Number of test number cases:");
+    scanf("%d",&T);
+    while(T--)
+    {
+        int a, b;
+        printf("Enter the value of a and b:");
+        scanf("%d %d",&a, &b);
+        int ans = a+b;
+        printf("\nAns: %d\n",ans);
+    }
+    return 0;
+}
