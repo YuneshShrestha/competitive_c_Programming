f28a91705e2c5aa37d888173d90a32a57dda9e06
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2021-11-15 23:30:50 +0545

    sum of 1st and last digit

--- main.c
+++ main.c
@@ -4,14 +4,19 @@ int main(void) {
 	int t,n;
 	scanf("%d", &t);
 	while(t--){
-		int sum = 0;
+		int sum = 0,f=0,l=0;
 		scanf("%d",&n);
-		while(n)
+		l += n%10;
+		while(n>0)
 		{
-			sum += n%10;
-			n /= 10;
+			f = n%10;
+			n /=10;
 		}
+		f = n;
+		sum = l + f;
 		printf("\n%d\n",sum);
+		
+		
 	}
 	
 }
