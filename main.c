afea9533207c62feeae391fce6c019e7ff6014c5
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2021-11-16 11:39:07 +0545

    lazy chef

--- main.c
+++ main.c
@@ -1,22 +1,31 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main(void) {
-	int t,n;
-	scanf("%d", &t);
-	while(t--){
-		int sum = 0,f=0,l=0;
-		scanf("%d",&n);
-		l += n%10;
-		while(n>0)
+int main(void)
+{
+	int t;
+	scanf("%d",&t);
+	while (t--)
+	{
+		int m,x,d,a1,a2;
+		scanf("%d",&x);
+		scanf("%d",&m);
+		scanf("%d",&d);
+		a1=m*x;
+		a2=d+x;
+		if(a1 != a2)
 		{
-			f = n%10;
-			n /=10;
+			if(a1<a2)
+			{
+				printf("\n%d\n",a1);
+			}
+			else{
+				printf("\n%d\n",a2);
+			}
 		}
-		f = n;
-		sum = l + f;
-		printf("\n%d\n",sum);
-		
-		
+		else{
+			printf("\n%d\n",a1);
+		}
 	}
 	
+	return 0;
 }
