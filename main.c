be434a35441398aa7b4cf2f7a6f50027b1658659
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2021-11-17 21:33:00 +0545

    counting digits in an array

--- main.c
+++ main.c
@@ -1,63 +1,26 @@
 #include<stdio.h>
 int main()
 {
-	int t;
-	scanf("%d",&t);
-	while (t--)
+	int a[10] = {0}, num, rem, i;
+	printf("Enter the number:\n");
+	scanf("%d",&num);
+	while (num)
 	{
-		int k;
-		scanf("%d",&k);
-		char ch[k][100];
-		int f=0;
-		for(int i = 0 ; i<k ; i++)
-		{
-			scanf("%s",ch[i]);
-		}
-		for (int i = 0; i < k; i++)
-		{
-
-			if (ch[i][0]>='a' && ch[i][0]<='m')
-			{
-				for (int j = 0; ch[i][j] != '\0'; j++)
-				{
-					if (!(ch[i][j]>='a' && ch[i][j]<='m'))
-					{
-						f =1;
-						break;
-					}
-				}
-			}
-			else if(ch[i][0]>='N' && ch[i][0]<='Z')
-			{
-				for (int j = 0; ch[i][j] != '\0'; j++)
-				{
-					if (!(ch[i][j]>='N' && ch[i][j]<='Z'))
-					{
-						f =1;
-						break;
-					}
-				}
-			}
-			else
-			{
-				f = 1;
-			}
-			
-			
-		}
-		if (f)
-		{
-			printf("No");
-		}
-		else
-		{
-			printf("Yes");
-		}
-		
-		
-		
-
+		rem = num % 10;
+		a[rem]++;
+		num /= 10;
 	}
+	printf("\n");
+	for (int i = 0; i < 10; i++)
+	{
+		if (a[i] != 0)
+		{
+			printf("Number = %d , Count = %d \n",a[i],i);
+		}
+		
+	}
+	
+
 	
 	return 0;
 }
