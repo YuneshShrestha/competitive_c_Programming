49b7daba1a52d272b27913d6039b068d475520b9
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2021-11-21 23:17:02 +0545

    finding factorial of large numbers

--- main.c
+++ main.c
@@ -1,53 +1,46 @@
 #include<stdio.h>
-int main(void)
+int main()
 {
-	int n=6;
-	int q=2;
-	int arr[1000]={0};
-	arr[0] = 1;
-	int len =1;
-	int x = 0;
-	int num =0;
-	scanf("%d",&n);
-	while(q!=n)
+	int t;
+	scanf("%d",&t);
+	while (t--)
 	{
-		x=0;
-		while(x<len)
-		{
-			//1. arr[0] becomes 2
-			// 2. arr[0] * 3 becomes 6
-			arr[x] = arr[x] * q; 
-			// 3. arr[0] * 4 becomes 24 so we are trying to store one digit in each array
-			// 4. arr[0] * 5 = 4*5 = 20  arr[1] * 5 = 2*5 =10
-			arr[x] += num; //arr[1] = 12
-			num = arr[x]/10; //3. returns 2    4. returns 2   returns 1
-			//3. storing 4 of 24 in arr[0]   4. arr[0] = 0
-			arr[x] = arr[x]%10;  // arr[1] = 2
-			// exiting loop
-			x++;
-		}
+		int n, q=2, len=1, arr[10000]={0},rem, num=0, x;
+		arr[0] = 1;
+		scanf("%d",&n);
 
-		while (num!=0)
+		while (q!=n+1)
 		{
-			//3. storing 2 of 24 in arr[1]
-			// 3. reminder of 2 will be 2
-			// 4. arr[2] = 1 
-			arr[len] = num%10;
-			//3. num = 0
-			// 4. num = 0
-			num /= 10;
-			//3. len= 2
-			// 4. len = 3
-			len++;
+			x=0;
+			while (x<len)
+			{
+				arr[x] = arr[x] * q;
+				arr[x] += num;
+				num = arr[x]/ 10;
+				arr[x] = arr[x] % 10;
+				x++;
+			}
+			while (num!=0)
+			{
+				arr[len] = num % 10;
+				num /= 10;
+				len++;
+			}
+			
+			q++;
+			
 		}
-		
-		q++;
-	}
-	len--;
-	while(len >= 0)
-	{
-		printf("%d",arr[len]);
 		len--;
+		while (len>=0)
+		{
+			printf("%d",arr[len]);
+			len--;
+
+		}
+		printf("\n");
 	}
 	
+	
+	
+	return 0;
 }
