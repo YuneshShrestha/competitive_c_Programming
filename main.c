71f18797d5f5bcb534d2e3dfdb0682104b4d09c9
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2021-11-25 22:08:03 +0545

    make equal

--- main.c
+++ main.c
@@ -2,25 +2,31 @@
 int main()
 {
 	int t;
-	scanf("%d", &t);
+	scanf("%d",&t);
 	while (t--)
 	{
-		int a,b,p,q;
-		scanf("%d %d %d %d", &a, &b, &p, &q);
-		if(a==p && b==q)
+		int n;
+		scanf("%d",&n);
+		int a[n], min ,max;
+		for (int i = 0; i < n; i++)
 		{
-			printf("0\n");
+			scanf("%d",&a[i]);
 		}
-		else if((a+b)%2==0 != (p+q)%2==0)
+		min=max=a[0];
+		for (int i = 0; i < n; i++)
 		{
-			printf("1\n");
+			if(a[i]>max)
+			{
+				max = a[i];
+			}
+			if(a[i]<min)
+			{
+				min = a[i];
+			}
 		}
-		else
-		{
-			printf("2\n");
-		}
-		
+		printf("%d", max-min);
 	}
+	
 	
 	return 0;
 }
