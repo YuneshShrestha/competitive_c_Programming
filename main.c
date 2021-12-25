6125814badd26d96f8447862cdebda4835586870
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2021-12-25 15:40:14 +0545

    petya and string

--- main.c
+++ main.c
@@ -1,33 +1,15 @@
 #include<stdio.h>
-#include <stdlib.h>
-int cmpfunc (const void * a, const void * b) {
-   return ( *(int*)b - *(int*)a );
-}
+#include <string.h>
 int main()
 {
-	int k, n, ans =0;
-	int s[1500];
-	scanf("%d %d",&k, &n);
-	for (int i = 0; i < k; i++)
-	{
-		scanf("%d",&s[i]);
-	}
-	qsort(s, k, sizeof(int), cmpfunc);
-
-	for (int i = 0; i < n; i++)
-	{
-		ans++;
-
-		for (int j = i; j < k; j++)
-		{
-			if(s[i] == s[j])
-			{
-				ans++;
-			}
-		}
-		i += ans;
-	}
-	printf("%d", ans);
+	char s[100], s1[100];
+	int ans;
 	
+	scanf("%s %s", s, s1);
+	strlwr(s);
+	strlwr(s1);
+	ans = strcmp(s, s1);
+
+	printf("%d",ans);
 	return 0;
 }
