69ae7e30f97ec02ec231c546fce01e09d06cbb5b
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2021-12-30 23:14:37 +0545

    new year

--- main.c
+++ main.c
@@ -1,15 +1,52 @@
 #include<stdio.h>
-#include <string.h>
+#include <stdlib.h>
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
+}
+
 int main()
 {
-	char s[100], s1[100];
-	int ans;
+	int year, store_year, flag =0;
+	int a[4] ,i=0;
+	scanf("%d",&year);
+	year++;
+	store_year = year;
+	// for (int i = 0; i < 10; i++)
+	// {
+	// 	year += 1;
+	// 	printf("%d",year);
+	// }
 	
-	scanf("%s %s", s, s1);
-	strlwr(s);
-	strlwr(s1);
-	ans = strcmp(s, s1);
-
-	printf("%d",ans);
+	while (1)
+	{
+		flag = 0;
+		store_year = year;
+		i=0;
+		// printf("%d %d\n", store_year, year);
+		while(store_year != 0)
+		{
+			a[i] = store_year % 10;
+			i++;
+			store_year /= 10;
+		}
+		qsort(a, 4, sizeof(int), cmpfunc);
+		for (int i = 0; i < 3; i++)
+		{
+			if (a[i] == a[i+1])
+			{
+				flag=1;
+				break;
+			}
+				
+		}
+		if (flag==0)
+		{
+			printf("%d", year);
+			break;
+		}
+		year += 1;
+		continue;
+	}
+	
 	return 0;
 }
